aa7819e11c3f5c0333954622858b532befa07328
Tony Chen <tonychen701@gmail.com>
2020-05-15 23:46:01 -0400

    added backupProgram and switch host file

--- Python/TelnetLib.py
+++ Python/TelnetLib.py
@@ -36,34 +36,71 @@ print(tn.read_all().decode('ascii'))
 
 """
 Program 2:
-    Telnet into switch to create vlan interfaces
+    Telnet into switches to create vlan interfaces
 """
 from getpass import getpass
 import telnetlib
 
-HOST = "192.168.122.72"
+HOST = ""
 user = input("Enter your telnet username: ")
 password = getpass("Enter your passowrd: ")
 
-tn = telnetlib.Telnet(HOST)
+f = open('myswitches')
 
-tn.read_until(b"Username: ")
-tn.write(user.encode('ascii') + b"\n")
-if password:
-    tn.read_until(b"Password: ")
-    tn.write(password.encode('ascii') + b"\n")
+for IP in f:
+    IP = IP.strip()
+    print ("Configuring Switch " + (IP))
+    HOST = IP
+    tn = telnetlib.Telnet(HOST)
+    tn.read_until(b"Username: ")
+    tn.write(user.encode('ascii') + b"\n")
+    if password:
+       tn.read_until(b"Password: ")
+       tn.write(password.encode('ascii') + b"\n")
+    tn.write(b"enable\n")
+    tn.write(b"cisco\n")
+    tn.write(b"conf t\n")
 
-tn.write(b"enable\n")
-tn.write(b"cisco\n")
-tn.write(b"conf t\n")
-
-for n in range (2,101):
+    for n in range (2,31):
     tn.write(b"vlan " + str(n).encode('ascii') + b"\n")
     tn.write(b"name Python_VLAN_" + str(n).encode('ascii') + b"\n")
 
-
-tn.write(b"end\n")
-tn.write(b"wr\n")
-tn.write(b"exit\n")
+    tn.write(b"end\n")
+    tn.write(b"wr\n")
+    tn.write(b"exit\n")
 
 print(tn.read_all().decode('ascii'))
+
+
+"""
+Program 3:
+    Backup switches configs 
+"""
+import getpass
+import telnetlib
+
+user = input('Enter your telnet username: ')
+password = getpass.getpass()
+
+f = open('myswitches')
+
+for IP in f:
+    IP=IP.strip()
+    print ('Get running config from Switch ' + (IP))
+    HOST = IP
+    tn = telnetlib.Telnet(HOST)
+    tn.read_until(b'Username: ')
+    tn.write(user.encode('ascii') + b'\n')
+    if password:
+        tn.read_until(b'Password: ')
+        tn.write(password.encode('ascii') + b'\n')  
+    tn.write(b"terminal length 0\n")
+    tn.write(b"show run\n")
+    tn.write(b'exit\n')
+
+    readoutput = tn.read_all()
+    saveoutput = open("switch " + HOST, "w")
+    saveoutput.write(readoutput.decode('ascii'))
+    saveoutput.write("\n")
+    saveoutput.close
+
